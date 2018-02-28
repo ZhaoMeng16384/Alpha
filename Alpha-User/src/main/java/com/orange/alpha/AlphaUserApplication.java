de51ec39a7b53bb573b4873d1a0f3f9573b15374
mengzhao <meng.zhao@bkjk.com>
2018-02-28 17:58:42 +0800

    Initial commit

---
+++ Alpha-User/src/main/java/com/orange/alpha/AlphaUserApplication.java
@@ -0,0 +1,12 @@
+package com.orange.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaUserApplication.class, args);
+	}
+}
