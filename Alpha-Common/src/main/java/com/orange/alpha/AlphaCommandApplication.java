1051708a43ad01aeda717a83750160587ca5ee67
mengzhao <meng.zhao@bkjk.com>
2018-03-01 17:10:34 +0800

    项目搭建1.1

---
+++ Alpha-Common/src/main/java/com/orange/alpha/AlphaCommandApplication.java
@@ -0,0 +1,12 @@
+package com.orange.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaCommandApplication.class, args);
+	}
+}
---
+++ Alpha-Common/src/main/java/com/orange/alpha/enumeration/BaseEnum.java
@@ -0,0 +1,13 @@
+package com.orange.alpha.enumeration;
+
+/**
+ * @Author: MengZhao
+ * @Descirption: 枚举的父类
+ * @Date: Created in 15:58 2018/3/1
+ */
+public interface BaseEnum {
+
+    int getCode();
+
+    String getMsg();
+}
