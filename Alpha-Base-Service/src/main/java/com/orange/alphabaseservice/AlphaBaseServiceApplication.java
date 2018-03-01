69334a599324025e9054f6f4ecbd30bf08ac599f
mengzhao <meng.zhao@bkjk.com>
2018-03-01 11:02:27 +0800

    项目搭建

---
+++ Alpha-Base-Service/src/main/java/com/orange/alphabaseservice/AlphaBaseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.orange.alphabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaBaseServiceApplication.class, args);
+	}
+}
